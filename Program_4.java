a6c28b641df1bc47a24490a781f56e451ef6d444
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-30 19:49:00 +0530

    Added Program_4

---
+++ Program_4.java
@@ -0,0 +1,12 @@
+public class Program_4 {
+    static int count;
+    static void display()
+    {
+        System.out.println("count value="+count);
+    }
+    public static void main(String args[])
+    {
+        count=10;
+        display();
+    }
+}
