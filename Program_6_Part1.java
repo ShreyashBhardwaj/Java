0a0e93aa22e9d382c79c327ad66101951c02f52e
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-30 19:57:10 +0530

    Program 6

---
+++ Program_6_Part1.java
@@ -0,0 +1,6 @@
+public class Program_6_Part1 {
+    void area() {
+        System.out.println("Area of shape is undefined");
+    }
+
+}
---
+++ Program_6_Part2.java
@@ -0,0 +1,10 @@
+public class Program_6_Part2 extends Program_6_Part1{
+    void area() {
+        System.out.println("Area of Circle = pi*r*r");
+    }
+    public static void main(String[] args) {
+        Program_6_Part1 s = new Program_6_Part2();
+        s.area();
+    }
+
+}
