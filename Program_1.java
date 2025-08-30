4db892ca4b23af5f707c33ac23cfbcbead7b2e1d
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-30 19:45:05 +0530

    Program_1.Java Done

--- Program_1.java
+++ Program_1.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Program_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        int result1 = a + b * c;
+        int result2 = (a + b) * c;
+        int result3 = a - b + c;
+        System.out.println("a + b * c = " + result1);
+        System.out.println("(a + b) * c = " + result2);
+        System.out.println("a - b + c = " + result3);
+    }
+
 }
