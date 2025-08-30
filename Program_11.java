045ccb0e4e60354cdfc51a95b3c36932efbf7a7b
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-30 20:48:09 +0530

    Added Program_11

---
+++ Program_11.java
@@ -0,0 +1,19 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Program_11 {
+    public static void main(String args[]) throws IOException {
+        FileInputStream fin = new FileInputStream("sample.txt");
+        FileOutputStream fout = new FileOutputStream("output.txt");
+
+        int ch;
+        while ((ch = fin.read()) != -1) {
+            fout.write(ch);
+        }
+
+        fin.close();
+        fout.close();
+        System.out.println("File copied successfully.");
+    }
+}
